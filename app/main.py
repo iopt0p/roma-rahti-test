36aa2cc98bfbfbd499b3c9be74e7333736e42d08
iopt0p <rkravchenko466@gmail.com>
2026-03-31 10:07:25 +0300

    Allowed CORS and changed endpoints a little

--- app/main.py
+++ app/main.py
@@ -1,14 +1,33 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
     return { "msg": "Hello Docker", "v": "0.2" }
 
 
-@app.get("/api/ip", response_class=HTMLResponse)
+@app.get("/api/ip")
+def read_ip(request: Request):
+    ip = request.client.host
+    return { "ip": ip }
+
+@app.get("/ip", response_class=HTMLResponse)
 def read_ip(request: Request):
     ip = request.client.host
     return f"<h1>Your IP is {ip}</h1>"
